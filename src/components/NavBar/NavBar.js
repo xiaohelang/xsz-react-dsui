070d885e452b7f199acb7b8974e6d268f1c436ba
heliang <heliang@zoomcommerce.net>
2018-12-14 17:29:11 +0800

    'navbar2'

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,75 @@
 import React, { Component } from 'react';
-import { Layout } from 'antd';
+import { Layout, Button, Icon, Avatar, Badge  } from 'antd';
 
 const { Sider, Content } = Layout;
 
 class NavBar extends Component {
+    constructor() {
+      super()
+      this.state= {
+        collapsed: false
+      }
+    }
+    // 全屏
+    toggleFullScreen = () => {
+      if (
+        !document.fullscreenElement &&
+        !document.mozFullScreenElement &&
+        !document.webkitFullscreenElement &&
+        !document.msFullscreenElement
+      ) {
+        if (document.documentElement.requestFullscreen) {
+          document.documentElement.requestFullscreen();
+        } else if (document.documentElement.msRequestFullscreen) {
+          document.documentElement.msRequestFullscreen();
+        } else if (document.documentElement.mozRequestFullScreen) {
+          document.documentElement.mozRequestFullScreen();
+        } else if (document.documentElement.webkitRequestFullscreen) {
+          document.documentElement.webkitRequestFullscreen(
+            Element.ALLOW_KEYBOARD_INPUT
+          );
+        }
+      } else {
+        if (document.exitFullscreen) {
+          document.exitFullscreen();
+        } else if (document.msExitFullscreen) {
+          document.msExitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+          document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+          document.webkitExitFullscreen();
+        }
+      }
+    }
+    // 收缩
+    toggleCollapsed = () => {
+      console.log('-------------09090-----')
+      this.setState({
+        collapsed:!this.state.collapsed
+      })
+    }
   render() {
     return (
-      <Layout>
+      <Layout style={{height: '100%'}}>
         <Sider theme='light' width={220}>聚合通关</Sider>
-        <Content height={64}>9999</Content>
+        <Content height={64} style={{background: '#fff', display: 'flex', justifyContent: 'space-between'}}>
+          <div>
+            <Button type="primary" onClick={this.toggleCollapsed} style={{ marginRight: 20 }}>
+              <Icon type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'} />
+            </Button>
+            <Icon  onClick={this.toggleFullScreen} type="fullscreen" />
+          </div>
+          <div>
+            <span style={{ marginRight: 20 }}>广州腾飞国际贸易公司</span>
+            <Badge count={1} size="small"  style={{ marginRight: 20 }}>
+              <a href="#">
+                <Icon type="bell" size="large"  theme="filled" style={{ marginRight: 26 }}/>
+              </a>
+            </Badge>
+            <Avatar icon="user" style={{ marginRight: 20 }}/>
+          </div>
+          {/* <Button onClick={this.toggleFullScreen}>全屏</Button> */}
+        </Content>
       </Layout>
     );
   }
--- src/components/SideBar/LeftSideBar.js
+++ src/components/SideBar/LeftSideBar.js
@@ -7,34 +7,43 @@ const { SubMenu, Item }= Menu;
 
 class LeftSideBar extends Component {
   render() {
+    console.log('inlineCollapsed')
+    console.log(this.props.collapsed)
+
+    const {collapsed} = this.props
+    
     return (
           <Sider width={220} theme='light' className="sidebar-left">
-            <Menu mode="inline" >
+            <Menu mode="inline" inlineCollapsed={collapsed} inlineIndent={24}>
               <Item key="1">
-                <Icon type="pie-chart" />
+                <Icon type="appstore" />
                 <span>工作台</span>
               </Item>
-              <SubMenu key="sub1" title={<span><Icon type="mail" /><span>单据管理</span></span>}>
+              <SubMenu key="sub1" title={<span><Icon type="desktop" /><span>单据管理</span></span>}>
                 <Menu.Item key="5">核注清单</Menu.Item>
                 <Menu.Item key="6">报关单</Menu.Item>
               </SubMenu>
-              <SubMenu key="sub2" title={<span><Icon type="mail" /><span>加工贸易</span></span>}>
+              <SubMenu key="sub2" title={<span><Icon type="profile" /><span>加工贸易</span></span>}>
                 <Menu.Item key="9">加贸手册备案</Menu.Item>
                 <Menu.Item key="10">加贸手册报核</Menu.Item>
-                <Menu.Item key="11">Option 7</Menu.Item>
-                <Menu.Item key="12">Option 8</Menu.Item>
-                <Menu.Item key="13">Option 8</Menu.Item>
-                <Menu.Item key="14">Option 8</Menu.Item>
-                <Menu.Item key="15">Option 8</Menu.Item>
-                <Menu.Item key="16">Option 8</Menu.Item>
-                <Menu.Item key="17">Option 8</Menu.Item>
-                <Menu.Item key="18">Option 8</Menu.Item>
-                <Menu.Item key="19">Option 8</Menu.Item>
-                <Menu.Item key="20">Option 8</Menu.Item>
-                <Menu.Item key="21">Option 8</Menu.Item>
-                <Menu.Item key="22">Option 8</Menu.Item>
-                <Menu.Item key="23">Option 8</Menu.Item>
-                <Menu.Item key="24">Option 8</Menu.Item>
+              </SubMenu>
+              <Item key="101">
+                <Icon type="file-text" />
+                <span>报表中心</span>
+              </Item>
+              <SubMenu key="sub3" title={<span><Icon type="inbox" /><span>基础资料</span></span>}>
+                <Menu.Item key="5">收货人</Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub4" title={<span><Icon type="setting" /><span>系统管理</span></span>}>
+                <Menu.Item key="5">我的信息</Menu.Item>
+                <Menu.Item key="6">公司信息</Menu.Item>
+                <Menu.Item key="7">用户信息</Menu.Item>
+                <Menu.Item key="8">权限管理</Menu.Item>
+                <Menu.Item key="9">预设值</Menu.Item>
+                <Menu.Item key="10">基础编码</Menu.Item>
+                <Menu.Item key="11">系统选项</Menu.Item>
+                <Menu.Item key="12">订单管理</Menu.Item>
+                <Menu.Item key="13">操作日记</Menu.Item>
               </SubMenu>
             </Menu>
           </Sider>
